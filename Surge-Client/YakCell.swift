7cdd080288bf6e22a3f8ce830ddd49c5f0cace56
Dustin Guerrero <2140378@gmail.com>
2015-04-16 23:13:16 -0700

    Add YakCell, YakTableViewController
     - Fix indentation (4 spaces to 2 spaces, per style guide)

---
+++ Surge-Client/YakCell.swift
@@ -0,0 +1,34 @@
+//
+//  TableViewCell.swift
+//  Surge-Client
+//
+//  Created by Dustin Guerrero on 4/16/15.
+//  Copyright (c) 2015 Team-Surge. All rights reserved.
+//
+
+import UIKit
+
+class YakCell: UITableViewCell {
+  
+  @IBOutlet weak var yakText: UILabel!
+  @IBOutlet weak var yakTime: UILabel!
+  @IBOutlet weak var karmaScore: UILabel!
+  @IBOutlet weak var replyCount: UILabel!
+  
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    // Initialization code
+    yakText.text = "<YakCell Default Text>"
+    yakTime.text = "<1m>"
+    karmaScore.text = "<\(999)>"
+    replyCount.text = "<99 replies>"
+  }
+  
+  override func setSelected(selected: Bool, animated: Bool) {
+    super.setSelected(selected, animated: animated)
+    
+    // Configure the view for the selected state
+  }
+  
+}
+ 
