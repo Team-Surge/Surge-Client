1522386f9c418d692c47a3c4bb45eee756dd961e
Dustin Guerrero <2140378@gmail.com>
2015-04-16 20:29:52 -0700

    Initial commit

---
+++ Surge-Client/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Surge-Client
+//
+//  Created by Dustin Guerrero on 4/16/15.
+//  Copyright (c) 2015 Team-Surge. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
