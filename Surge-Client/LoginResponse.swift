a1b06e063e25e857c5975fb35f664d6171f59e97
omoman <mannysanchez998@gmail.com>
2015-05-03 22:20:00 -0700

    Add login response json decoder

---
+++ Surge-Client/LoginResponse.swift
@@ -0,0 +1,22 @@
+//
+//  LoginResponse.swift
+//  Surge-Client
+//
+//  Created by Manuel Sanchez Munoz on 5/3/15.
+//  Copyright (c) 2015 Team-Surge. All rights reserved.
+//
+
+import Foundation
+import JSONJoy
+
+struct LoginResponse: JSONJoy {
+  var action: String?
+  var success: Bool?
+  var reasons: Dictionary<String, Array<String> >?
+  
+  init(_ decoder: JSONDecoder) {
+    action = decoder["action"].string
+    success = decoder["success"].bool
+    decoder.getDictionary(&reasons)
+  }
+}
