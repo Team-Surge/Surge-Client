946cab8d1c993cd28a97c4076033a3898ec5d359
omoman <mannysanchez998@gmail.com>
2015-05-04 01:39:50 -0700

    Add register functionality
    - Add spinner while authenticating
    - Add basic email authentication
    - Show toast on failure and success

--- Surge-Client/RegisterViewController.swift
+++ Surge-Client/RegisterViewController.swift
@@ -7,9 +7,39 @@
 //
 
 import UIKit
+import CRToast
+import SwiftHTTP
+import JSONJoy
 
 class RegisterViewController: UIViewController {
-
+  @IBOutlet weak var emailField: UITextField!
+  @IBOutlet weak var passwordField: UITextField!
+  @IBOutlet weak var reenterPasswordField: UITextField!
+  @IBOutlet weak var registerSpinner: UIActivityIndicatorView!
+  
+  var registrationEmailFailureToastOptions: [NSObject:AnyObject] = [
+    kCRToastNotificationTypeKey: CRToastType.NavigationBar.rawValue,
+    // Set this before dispatching the alert
+    kCRToastTextKey: "" as NSString,
+    kCRToastTextAlignmentKey: NSTextAlignment.Center.rawValue,
+    kCRToastBackgroundColorKey: UIColor.redColor(),
+    kCRToastAnimationInTypeKey: CRToastAnimationType.Linear.rawValue,
+    kCRToastAnimationOutTypeKey: CRToastAnimationType.Linear.rawValue,
+    kCRToastAnimationInDirectionKey: CRToastAnimationDirection.Top.rawValue,
+    kCRToastAnimationOutDirectionKey: CRToastAnimationDirection.Top.rawValue
+  ]
+  
+  let registrationEmailSuccessToastOptions: [NSObject:AnyObject] = [
+    kCRToastNotificationTypeKey: CRToastType.NavigationBar.rawValue,
+    kCRToastTextKey: "Registration was successful!" as NSString,
+    kCRToastTextAlignmentKey: NSTextAlignment.Center.rawValue,
+    kCRToastBackgroundColorKey: UIColor.greenColor(),
+    kCRToastAnimationInTypeKey: CRToastAnimationType.Linear.rawValue,
+    kCRToastAnimationOutTypeKey: CRToastAnimationType.Linear.rawValue,
+    kCRToastAnimationInDirectionKey: CRToastAnimationDirection.Top.rawValue,
+    kCRToastAnimationOutDirectionKey: CRToastAnimationDirection.Top.rawValue
+  ]
+  
   override func viewDidLoad() {
     super.viewDidLoad()
 
@@ -20,7 +50,54 @@ class RegisterViewController: UIViewController {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
+  
+  func hasValidRegistrationInput() -> Bool {
+    if passwordField.text != reenterPasswordField.text {
+      registrationEmailFailureToastOptions[kCRToastTextKey] = "Passwords do not match"
+      CRToastManager.showNotificationWithOptions(registrationEmailFailureToastOptions, completionBlock:
+        {_ in})
+      return false
+    }
+    return true
+  }
 
+  @IBAction func registerAction(sender: UIButton) {
+    var request = HTTPTask()
+    let params: [String:AnyObject] = [
+      "action":"userCreate",
+      "email":emailField.text,
+      "password":passwordField.text
+    ]
+    
+    if hasValidRegistrationInput() == false {
+      return
+    }
+    
+    registerSpinner.startAnimating()
+    
+    request.POST("http://surge.seektom.com/auth", parameters: params,
+      success: {(response: HTTPResponse) in
+        let jsonResponse = RegisterResponse(JSONDecoder(response.responseObject!))
+        if jsonResponse.success! == true {
+          dispatch_async(dispatch_get_main_queue(), {
+            CRToastManager.showNotificationWithOptions(self.registrationEmailSuccessToastOptions,
+              completionBlock: {_ in})
+            self.dismissViewControllerAnimated(true, completion: nil)
+          })
+        } else {
+          dispatch_async(dispatch_get_main_queue(), {
+            self.registerSpinner.stopAnimating()
+            self.registrationEmailFailureToastOptions[kCRToastTextKey] = "Registration failure"
+            CRToastManager.showNotificationWithOptions(self.registrationEmailFailureToastOptions,
+              completionBlock: {_ in})
+          })
+        }
+      }, failure: {(error: NSError, response: HTTPResponse?) in
+      
+      })
+    
+  }
+  
   @IBAction func backToLoginAction(sender: UIButton) {
     self.dismissViewControllerAnimated(true, completion: nil)
   }
