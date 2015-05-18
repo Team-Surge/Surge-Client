Team-Surge/Surge-Client

commit 85e404fd00939c90f03898b659a24b60757d6726

Create YakPostViewController and YakPostDetailViewController
* Move cell logic from YakDetailView and YakMainView to new
  controllers

Surge-Client/YakPostViewController.swift
@@ -1,0 +1,118 @@
+//
+//  YakPostViewController.swift
+//  Surge-Client
+//
+//  Created by Dustin Guerrero on 5/17/15.
+//  Copyright (c) 2015 Team-Surge. All rights reserved.
+//
+
+import UIKit
+import SwiftHTTP
+import JSONJoy
+
+
+
+protocol YakPostViewControllerSource {
+  func generatePostRetrieveParameters() -> Dictionary<String, String>
+  func postWasSelected(post: Post)
+  //func generatePostResponseParameters(sender: Post) -> Dictionary<String, String>
+  //func generatePostViewParameters() -> Dictionary<String, String>
+}
+
+class YakPostViewController: UITableViewController {
+  internal var posts = Array<Post>()
+  internal var delegate: YakPostViewControllerSource?
+  
+  internal func retrievePosts() {
+    if let delegate = self.delegate {
+      let request = HTTPTask()
+      let params: Dictionary<String,String> = delegate.generatePostRetrieveParameters()
+      
+      request.POST("http://surge.seektom.com/post", parameters: params,
+        success: {(response: HTTPResponse) in
+          if response.responseObject != nil {
+            let resp = PostResponse(JSONDecoder(response.responseObject!))
+            self.posts.removeAll(keepCapacity: true)
+            for post in resp.posts {
+              self.posts.insert(post, atIndex: 0)
+            }
+            dispatch_async(dispatch_get_main_queue(),{
+              self.tableView.reloadData()
+              self.refreshControl!.endRefreshing()
+            })
+          }
+        }, failure: {(error: NSError, response: HTTPResponse?) in
+          println("[YakMainViewController] Update Failed with error\n\t\(error)")
+          dispatch_async(dispatch_get_main_queue(),{
+            self.refreshControl!.endRefreshing()
+          })
+        }
+      )
+    }
+  }
+  
+  func sortCellsByHot() {
+    posts.sort({$0.voteCount > $1.voteCount})
+  }
+  
+  func sortCellsByRecent() {
+    posts.sort({$0.timestamp.timeIntervalSinceReferenceDate > $1.timestamp.timeIntervalSinceReferenceDate})
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    refreshControl = UIRefreshControl()
+    refreshControl!.addTarget(self, action: Selector("retrievePosts"), forControlEvents: UIControlEvents.ValueChanged)
+    refreshControl!.backgroundColor = UIColor.whiteColor()
+    refreshControl!.tintColor = UIColor.blackColor()
+    
+    
+    tableView.registerNib(UINib(nibName: "YakCell", bundle: nil), forCellReuseIdentifier: "YakCell")
+    retrievePosts()
+  }
+  
+  override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCellWithIdentifier("YakCell", forIndexPath: indexPath) as! YakCell
+    let post = self.posts[indexPath.row]
+    
+    cell.initializeCellFromPost(post)
+    cell.delegate = self
+    return cell
+  }
+  
+  override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return posts.count
+  }
+  
+  override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+    delegate?.postWasSelected(posts[indexPath.row])
+  }
+  
+  
+  override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+    return 1
+  }
+  
+  override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+    return UITableViewAutomaticDimension
+  }
+
+  override func tableView(tableView: UITableView, estimatedHeightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+    return 80
+  }
+}
+
+extension YakPostViewController: YakCellDelegate {
+  func cellDidChangeVoteState(cell: YakCell, state: VoteState) {
+    let request = HTTPTask()
+    var params: Dictionary<String,AnyObject> = ["action": "postVote", "postId": cell.id, "direction": state.rawValue]
+    
+    
+    request.POST("http://surge.seektom.com/post", parameters: params,
+      success: {(response: HTTPResponse) in
+      }, failure: {(error: NSError, response: HTTPResponse?) in
+          println("[YakMainViewController] Vote Failed with error:\n\t\(error)")
+      }
+    )
+  }
+}

Surge-Client.xcodeproj/project.pbxproj
@@ -19,6 +19,7 @@
 		3FB0BDAF1AEA1E23009A8DB0 /* MeViewController.swift in Sources */ = {isa = PBXBuildFile; fileRef = 3FB0BDAE1AEA1E23009A8DB0 /* MeViewController.swift */; };
 		3FCCBDD31AF75C0900D1695A /* RegisterResponse.swift in Sources */ = {isa = PBXBuildFile; fileRef = 3FCCBDD21AF75C0900D1695A /* RegisterResponse.swift */; };
 		65C111CACFEFFEA9FD86FB1C /* Pods_Surge_Client.framework in Frameworks */ = {isa = PBXBuildFile; fileRef = 63D58DF9116783E77F98DB42 /* Pods_Surge_Client.framework */; settings = {ATTRIBUTES = (Required, ); }; };
+		DA2F086F1B099E6C0061B318 /* YakPostViewController.swift in Sources */ = {isa = PBXBuildFile; fileRef = DA2F086E1B099E6C0061B318 /* YakPostViewController.swift */; };
 		DA5533481AE0B57F003E369E /* AppDelegate.swift in Sources */ = {isa = PBXBuildFile; fileRef = DA5533471AE0B57F003E369E /* AppDelegate.swift */; };
 		DA55334B1AE0B57F003E369E /* Surge_Client.xcdatamodeld in Sources */ = {isa = PBXBuildFile; fileRef = DA5533491AE0B57F003E369E /* Surge_Client.xcdatamodeld */; };
 		DA5533501AE0B57F003E369E /* Main.storyboard in Resources */ = {isa = PBXBuildFile; fileRef = DA55334E1AE0B57F003E369E /* Main.storyboard */; };
@@ -40,6 +41,7 @@
 		DAD2E4441AE7405A00DFBB53 /* YakCell.xib in Resources */ = {isa = PBXBuildFile; fileRef = DAD2E4431AE7405A00DFBB53 /* YakCell.xib */; };
 		DAD498941AE0C114000E5E93 /* YakCell.swift in Sources */ = {isa = PBXBuildFile; fileRef = DAD498931AE0C114000E5E93 /* YakCell.swift */; };
 		DAE725E41AF036FA008B8DA3 /* LocationManager.m in Sources */ = {isa = PBXBuildFile; fileRef = DAE725E31AF036FA008B8DA3 /* LocationManager.m */; };
+		DAF211ED1B09D9B400EEEE4A /* YakPostDetailViewController.swift in Sources */ = {isa = PBXBuildFile; fileRef = DAF211EC1B09D9B400EEEE4A /* YakPostDetailViewController.swift */; };
 /* End PBXBuildFile section */
 
 /* Begin PBXContainerItemProxy section */
@@ -80,6 +82,7 @@
 		3FCCBDD21AF75C0900D1695A /* RegisterResponse.swift */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.swift; path = RegisterResponse.swift; sourceTree = "<group>"; };
 		42FD031BEDF5185BD1CDFBA1 /* Pods-Surge-Client.release.xcconfig */ = {isa = PBXFileReference; includeInIndex = 1; lastKnownFileType = text.xcconfig; name = "Pods-Surge-Client.release.xcconfig"; path = "Pods/Target Support Files/Pods-Surge-Client/Pods-Surge-Client.release.xcconfig"; sourceTree = "<group>"; };
 		63D58DF9116783E77F98DB42 /* Pods_Surge_Client.framework */ = {isa = PBXFileReference; explicitFileType = wrapper.framework; includeInIndex = 0; path = Pods_Surge_Client.framework; sourceTree = BUILT_PRODUCTS_DIR; };
+		DA2F086E1B099E6C0061B318 /* YakPostViewController.swift */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.swift; path = YakPostViewController.swift; sourceTree = "<group>"; };
 		DA5533421AE0B57F003E369E /* Surge-Client.app */ = {isa = PBXFileReference; explicitFileType = wrapper.application; includeInIndex = 0; path = "Surge-Client.app"; sourceTree = BUILT_PRODUCTS_DIR; };
 		DA5533461AE0B57F003E369E /* Info.plist */ = {isa = PBXFileReference; lastKnownFileType = text.plist.xml; path = Info.plist; sourceTree = "<group>"; };
 		DA5533471AE0B57F003E369E /* AppDelegate.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = AppDelegate.swift; sourceTree = "<group>"; };
@@ -105,6 +108,7 @@
 		DAE725E21AF036FA008B8DA3 /* LocationManager.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = LocationManager.h; sourceTree = "<group>"; };
 		DAE725E31AF036FA008B8DA3 /* LocationManager.m */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.objc; path = LocationManager.m; sourceTree = "<group>"; };
 		DAE725E51AF03774008B8DA3 /* Surge-Client-Bridging-Header.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = "Surge-Client-Bridging-Header.h"; sourceTree = "<group>"; };
+		DAF211EC1B09D9B400EEEE4A /* YakPostDetailViewController.swift */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.swift; path = YakPostDetailViewController.swift; sourceTree = "<group>"; };
 		FC690B71D53F3FD7E5BC761C /* Pods-Surge-Client.debug.xcconfig */ = {isa = PBXFileReference; includeInIndex = 1; lastKnownFileType = text.xcconfig; name = "Pods-Surge-Client.debug.xcconfig"; path = "Pods/Target Support Files/Pods-Surge-Client/Pods-Surge-Client.debug.xcconfig"; sourceTree = "<group>"; };
 /* End PBXFileReference section */
 
@@ -148,6 +152,8 @@
 				DAD2E43D1AE738A700DFBB53 /* YakDetailViewController.swift */,
 				DAD2E43B1AE7374100DFBB53 /* YakMainViewController.swift */,
 				DAC2D0EC1AEF1EEE0047E4AE /* YakCreatePostViewController.swift */,
+				DA2F086E1B099E6C0061B318 /* YakPostViewController.swift */,
+				DAF211EC1B09D9B400EEEE4A /* YakPostDetailViewController.swift */,
 				DAD498931AE0C114000E5E93 /* YakCell.swift */,
 				DAD2E4431AE7405A00DFBB53 /* YakCell.xib */,
 			);
@@ -461,8 +467,10 @@
 			buildActionMask = 2147483647;
 			files = (
 				3F9A3D741AEF1895006E4B70 /* LoginViewController.swift in Sources */,
+				DAF211ED1B09D9B400EEEE4A /* YakPostDetailViewController.swift in Sources */,
 				3FB0BDAF1AEA1E23009A8DB0 /* MeViewController.swift in Sources */,
 				DACDB9551B0331040074C4AD /* PostResponse.swift in Sources */,
+				DA2F086F1B099E6C0061B318 /* YakPostViewController.swift in Sources */,
 				DAD498941AE0C114000E5E93 /* YakCell.swift in Sources */,
 				3F20A3031AE98134004C2598 /* NearTableViewController.swift in Sources */,
 				3FB0BDAD1AE9FCAD009A8DB0 /* NotificationsTableViewController.swift in Sources */,

Surge-Client/YakCell.swift
@@ -128,7 +128,6 @@ class YakCell: UITableViewCell {
     
     if let handle = post.handle {
       if handle == "" {
-        println("Hiding handle")
         handleLabel.hidden = true
         contentVerticalSpace.constant = 5
         if let x = centerAlignmentConstraint {
@@ -139,7 +138,6 @@ class YakCell: UITableViewCell {
         handleLabel.hidden = false
       }
     } else {
-      println("Hiding handle")
       handleLabel.hidden = true
     }
     

Surge-Client/YakDetailViewController.swift
@@ -14,16 +14,17 @@ import JSONJoy
 
 class YakDetailViewController: UIViewController {
 
-  @IBOutlet weak var innerTableView: UITableView?
   @IBOutlet weak var mapView: MKMapView!
   @IBOutlet weak var bottomConstraint: NSLayoutConstraint!
   @IBOutlet weak var sendButton: UIButton!
   @IBOutlet weak var textField: UITextField!
   @IBOutlet weak var textView: UITextView!
   
-  var originalBottomConstraintConstant: CGFloat!
   var sourcePost: Post!
-  private var comments = Array<Post>()
+  
+  internal var originalBottomConstraintConstant: CGFloat!
+  internal var comments = Array<Post>()
+  internal var postViewController: YakPostViewController?
   
   @IBAction func onSendButtonPress(sender: AnyObject) {
     textFieldShouldReturn(textField)
@@ -40,10 +41,6 @@ class YakDetailViewController: UIViewController {
     mapView.delegate = self
     
 
-    // Setup table cells
-    innerTableView?.registerNib(UINib(nibName: "YakCell", bundle: nil), forCellReuseIdentifier: "YakCell")
-    innerTableView?.delegate = self
-
     // Setup text field and keyboard
     textField.delegate = self
     NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("keyboardWillShow:"), name:UIKeyboardWillShowNotification, object: nil)
@@ -54,7 +51,6 @@ class YakDetailViewController: UIViewController {
   override func viewWillAppear(animated: Bool) {
     super.viewWillDisappear(animated)
     LocationManager.sharedInstance().addLocationManagerDelegate(self)
-    updatePosts()
   }
   
   override func viewDidDisappear(animated: Bool) {
@@ -79,61 +75,15 @@ class YakDetailViewController: UIViewController {
     originalBottomConstraintConstant = bottomConstraint.constant
     bottomConstraint.constant = keyboardFrame.size.height + bottomConstraint.constant
   }
-  
-  func updatePosts() {
-    let request = HTTPTask()
-    let params: Dictionary<String,AnyObject> = ["action": "postDetail", "postId": sourcePost.id!]
-    
-    request.POST("http://surge.seektom.com/post", parameters: params,
-      success: {(response: HTTPResponse) in
-        if response.responseObject != nil {
-          let resp = PostDetailResponse(JSONDecoder(response.responseObject!))
-          self.comments.removeAll(keepCapacity: true)
-          for comment in resp.post.comments! {
-            self.comments.append(comment)
-          }
-          dispatch_async(dispatch_get_main_queue(),{
-            self.innerTableView?.reloadData()
-          })
-        }
-      }, failure: {(error: NSError, response: HTTPResponse?) in
-        println("[YakDetailViewController] Update Failed with error\n\t\(error)")
-      }
-    )
-  }
 
+  override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    if segue.identifier == "embedSegue" {
+      let postViewController = segue.destinationViewController as! YakPostViewController
+      postViewController.delegate = self
+    }
+  }
 }
 
-// MARK: - UITableViewDelegate
-extension YakDetailViewController: UITableViewDelegate {
-  
-  func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCellWithIdentifier("YakCell", forIndexPath: indexPath) as! YakCell
-    let post = self.comments[indexPath.row]
-    
-    cell.initializeCellFromPost(post)
-    cell.delegate = self
-    return cell
-  }
-  
-}
-
-// MARK: - UITableViewDataSource
-extension YakDetailViewController: UITableViewDataSource {
-  
-  func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
-    return 100
-  }
-  
-  func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return comments.count
-  }
-  
-  func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-    return 1
-  }
-  
-}
 
 // MARK: - UITextFieldDelegate
 extension YakDetailViewController: UITextFieldDelegate {
@@ -144,7 +94,7 @@ extension YakDetailViewController: UITextFieldDelegate {
     
     request.POST("http://surge.seektom.com/post", parameters: params,
       success: {(response: HTTPResponse) in
-          dispatch_async(dispatch_get_main_queue(),{self.updatePosts()})
+          dispatch_async(dispatch_get_main_queue(),{})
       }, failure: {(error: NSError, response: HTTPResponse?) in
         println("[YakDetailViewController] Update Failed with error\n\t\(error)")
       }
@@ -167,17 +117,12 @@ extension YakDetailViewController: LocationManagerDelegate {
   }
 }
 
-// MARK: - YakCellDelegate
-extension YakDetailViewController: YakCellDelegate {
-  func cellDidChangeVoteState(cell: YakCell, state: VoteState) {
-    let request = HTTPTask()
-    var params: Dictionary<String,AnyObject> = ["action": "postCommentVote", "commentId": cell.id, "direction": state.rawValue]
-    
-    request.POST("http://surge.seektom.com/post", parameters: params,
-      success: {(response: HTTPResponse) in
-      }, failure: {(error: NSError, response: HTTPResponse?) in
-          println("[YakDetailViewController] Vote Failed with error:\n\t\(error)")
-      }
-    )
+extension YakDetailViewController: YakPostViewControllerSource {
+  func generatePostRetrieveParameters() -> Dictionary<String, String> {
+    println("Getting: \(sourcePost.id)")
+    return ["action": "postDetail", "postId": toString(sourcePost.id)]
+  }
+  
+  func postWasSelected(post: Post) {
   }
 }

Surge-Client/YakMainViewController.swift
@@ -12,130 +12,37 @@ import SwiftHTTP
 import JSONJoy
 
 class YakMainViewController: UIViewController {
-  
-  @IBOutlet weak var innerTableView: UITableView!
-  
-  private var posts = Array<Post>()
-  private let refreshControl = UIRefreshControl()
+  internal var postViewController: YakPostViewController?
   
   @IBAction func sortButtonChanged(sender: UISegmentedControl) {
     if sender.selectedSegmentIndex == 1 {
       // Sort by hot
-      posts.sort({$0.voteCount > $1.voteCount})
+      postViewController?.sortCellsByHot()
     } else {
       // Sort by new
-      posts.sort({$0.timestamp.timeIntervalSinceReferenceDate > $1.timestamp.timeIntervalSinceReferenceDate})
+      postViewController?.sortCellsByRecent()
     }
-    self.innerTableView.reloadData()
   }
   
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    refreshControl.addTarget(self, action: Selector("updatePosts"), forControlEvents: UIControlEvents.ValueChanged)
-    refreshControl.backgroundColor = UIColor.whiteColor()
-    refreshControl.tintColor = UIColor.blackColor()
-    
-    
-    innerTableView.addSubview(refreshControl)
-    innerTableView.registerNib(UINib(nibName: "YakCell", bundle: nil), forCellReuseIdentifier: "YakCell")
-  }
-  
-  
-  override func viewWillAppear(animated: Bool) {
-    updatePosts()
-  }
-  
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-  }
   
   override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
     if segue.identifier == "detailViewSegue" {
       let destination = segue.destinationViewController as! YakDetailViewController
       destination.sourcePost = sender as! Post
+    } else if segue.identifier == "embedSegue" {
+      postViewController = segue.destinationViewController as? YakPostViewController
+      postViewController?.delegate = self
     }
   }
   
-  func updatePosts() {
-    let request = HTTPTask()
-    let params: Dictionary<String,AnyObject> = ["action": "postList"]
-    
-    request.POST("http://surge.seektom.com/post", parameters: params,
-      success: {(response: HTTPResponse) in
-        if response.responseObject != nil {
-          let resp = PostResponse(JSONDecoder(response.responseObject!))
-          self.posts.removeAll(keepCapacity: true)
-          for post in resp.posts {
-            self.posts.insert(post, atIndex: 0)
-          }
-          dispatch_async(dispatch_get_main_queue(),{
-            self.innerTableView.reloadData()
-            self.refreshControl.endRefreshing()
-          })
-        }
-      }, failure: {(error: NSError, response: HTTPResponse?) in
-        println("[YakMainViewController] Update Failed with error\n\t\(error)")
-        dispatch_async(dispatch_get_main_queue(),{
-          self.refreshControl.endRefreshing()
-        })
-      }
-    )
-  }
 }
 
-
-// MARK: - UITableViewDelegate
-extension YakMainViewController: UITableViewDelegate {
-  
-  func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-    performSegueWithIdentifier("detailViewSegue", sender: posts[indexPath.row])
-  }
-
-}
-
-
-// MARK: - UITableViewDataSource
-extension YakMainViewController: UITableViewDataSource {
-  
-  func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return posts.count
+extension YakMainViewController: YakPostViewControllerSource {
+  func generatePostRetrieveParameters() -> Dictionary<String, String> {
+    return ["action": "postList"]
   }
   
-  
-  func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCellWithIdentifier("YakCell", forIndexPath: indexPath) as! YakCell
-    let post = self.posts[indexPath.row]
-    
-    cell.initializeCellFromPost(post)
-    cell.delegate = self
-    return cell
-  }
-  
-  func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-    return 1
-  }
-  
-  func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
-    return UITableViewAutomaticDimension
-  }
-
-  func tableView(tableView: UITableView, estimatedHeightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
-    return UITableViewAutomaticDimension
-  }
-}
-
-// MARK: - YakCellDelegate
-extension YakMainViewController: YakCellDelegate {
-  func cellDidChangeVoteState(cell: YakCell, state: VoteState) {
-    let request = HTTPTask()
-    var params: Dictionary<String,AnyObject> = ["action": "postVote", "postId": cell.id, "direction": state.rawValue]
-    
-    
-    request.POST("http://surge.seektom.com/post", parameters: params,
-      success: {(response: HTTPResponse) in
-      }, failure: {(error: NSError, response: HTTPResponse?) in
-          println("[YakMainViewController] Vote Failed with error:\n\t\(error)")
-      }
-    )
+  func postWasSelected(post: Post) {
+    performSegueWithIdentifier("detailViewSegue", sender: post)
   }
 }

Surge-Client/YakPostDetailViewController.swift
@@ -1,0 +1,41 @@
+//
+//  YakPostDetailViewController.swift
+//  Surge-Client
+//
+//  Created by Dustin Guerrero on 5/18/15.
+//  Copyright (c) 2015 Team-Surge. All rights reserved.
+//
+
+
+import SwiftHTTP
+import JSONJoy
+
+class YakPostDetailViewController: YakPostViewController {
+  internal override func retrievePosts() {
+    if let delegate = self.delegate {
+      let request = HTTPTask()
+      let params: Dictionary<String,String> = delegate.generatePostRetrieveParameters()
+      
+      request.POST("http://surge.seektom.com/post", parameters: params,
+        success: {(response: HTTPResponse) in
+          if response.responseObject != nil {
+            let resp = PostDetailResponse(JSONDecoder(response.responseObject!))
+            self.posts.removeAll(keepCapacity: true)
+            for post in resp.post.comments! {
+              self.posts.insert(post, atIndex: 0)
+            }
+            dispatch_async(dispatch_get_main_queue(),{
+              self.tableView.reloadData()
+              self.refreshControl!.endRefreshing()
+            })
+          }
+        }, failure: {(error: NSError, response: HTTPResponse?) in
+          println("[YakMainViewController] Update Failed with error\n\t\(error)")
+          dispatch_async(dispatch_get_main_queue(),{
+            self.refreshControl!.endRefreshing()
+          })
+        }
+      )
+    }
+  }
+}

commit 9f63e2a9ce171b8bcf20686f78fa98d767afe45f

Fix sorting broken in 85e404fd00939c90f03898b659a24b60757d6726

Surge-Client/YakPostViewController.swift
@@ -53,10 +53,12 @@ class YakPostViewController: UITableViewController {
   
   func sortCellsByHot() {
     posts.sort({$0.voteCount > $1.voteCount})
+    tableView.reloadData()
   }
   
   func sortCellsByRecent() {
     posts.sort({$0.timestamp.timeIntervalSinceReferenceDate > $1.timestamp.timeIntervalSinceReferenceDate})
+    tableView.reloadData()
   }
   
   override func viewDidLoad() {