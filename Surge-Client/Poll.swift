0a7733d982d58c11c586fbfd99c56cb3e27573a1
omoman <mannysanchez998@gmail.com>
2015-05-18 18:13:15 -0700

    Objects: Add Poll file (doesn't do anything yet)

---
+++ Surge-Client/Poll.swift
@@ -0,0 +1,23 @@
+//
+//  Poll.swift
+//  Surge-Client
+//
+//  Created by Manuel Sanchez Munoz on 5/18/15.
+//  Copyright (c) 2015 Team-Surge. All rights reserved.
+//
+
+import Foundation
+import JSONJoy
+
+class Poll : Post {
+  /*
+    Add Poll specific vars here...
+   */
+
+  required init(_ decoder: JSONDecoder) {
+    super.init(decoder)
+    /*
+      Initialize Poll specific vars here...
+     */
+  }
+}
