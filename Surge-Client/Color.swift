8c3e7988fa1e4287d655831b980604ec41137881
omoman <mannysanchez998@gmail.com>
2015-05-18 21:20:29 -0700

    Add Colors to use throughout project
    * Use complementary colors to Surge purple
    * Use bootstrap colors for success and error
    * Add UIColorExtension.swift to convert HTML colors to UIColors easily

---
+++ Surge-Client/Color.swift
@@ -0,0 +1,25 @@
+//
+//  Color.swift
+//  Surge-Client
+//
+//  Created by Manuel Sanchez Munoz on 5/18/15.
+//  Copyright (c) 2015 Team-Surge. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Color {
+  // Shades of purple
+  let Surge = UIColor(rgba: "#a56cfd")
+  let Lightning = UIColor(rgba: "#b686ff")
+  let Blurple = UIColor(rgba: "#683ab0")
+  
+  // Shades of Green
+  let Leaf = UIColor(rgba: "#b0ac28")
+  let KeyLime = UIColor(rgba: "#fdf86C")
+  
+  // Bootstrap Colors
+  let Danger = UIColor(rgba: "#d9534f")
+  let Success = UIColor(rgba: "#5cb85c")
+}
