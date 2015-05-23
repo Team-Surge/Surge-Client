ed0882dcc266d58535a92ffe85e8b2161b8b6315
Dustin Guerrero <2140378@gmail.com>
2015-05-22 20:43:02 -0700

    Add SurgeSocket

---
+++ Surge-Client/SurgeSocket.swift
@@ -0,0 +1,13 @@
+//
+//  SurgeSocket.swift
+//  Surge-Client
+//
+//  Created by Dustin Guerrero on 5/21/15.
+//  Copyright (c) 2015 Team-Surge. All rights reserved.
+//
+
+import UIKit
+
+class SurgeSocket: NSObject {
+   
+}
