2de36e673e616daed643b63de8d5f9ed2fd2dca4
omoman <mannysanchez998@gmail.com>
2015-05-26 00:20:00 -0700

    Add INTULocationManager to pod and add LoadingScreen implementation

--- Pods/Target Support Files/Pods-Surge-Client/Pods-Surge-Client-frameworks.sh
+++ Pods/Target Support Files/Pods-Surge-Client/Pods-Surge-Client-frameworks.sh
@@ -17,8 +17,8 @@ install_framework()
   fi
 
   # use filter instead of exclude so missing patterns dont' throw errors
-  echo "rsync -av --filter "- CVS/" --filter "- .svn/" --filter "- .git/" --filter "- .hg/" --filter "- Headers/" --filter "- PrivateHeaders/" --filter "- Modules/" ${source} ${destination}"
-  rsync -av --filter "- CVS/" --filter "- .svn/" --filter "- .git/" --filter "- .hg/" --filter "- Headers/" --filter "- PrivateHeaders/" --filter "- Modules/" "${source}" "${destination}"
+  echo "rsync -av --filter "- CVS/" --filter "- .svn/" --filter "- .git/" --filter "- .hg/" --filter "- Headers/" --filter "- PrivateHeaders/" ${source} ${destination}"
+  rsync -av --filter "- CVS/" --filter "- .svn/" --filter "- .git/" --filter "- .hg/" --filter "- Headers/" --filter "- PrivateHeaders/" "${source}" "${destination}"
   # Resign the code if required by the build settings to avoid unstable apps
   if [ "${CODE_SIGNING_REQUIRED}" == "YES" ]; then
       code_sign "${destination}/$1"
@@ -49,6 +49,7 @@ code_sign() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework 'CRToast.framework'
+  install_framework 'INTULocationManager.framework'
   install_framework 'JSONJoy.framework'
   install_framework 'JSQMessagesViewController.framework'
   install_framework 'JSQSystemSoundPlayer.framework'
@@ -56,6 +57,7 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework 'CRToast.framework'
+  install_framework 'INTULocationManager.framework'
   install_framework 'JSONJoy.framework'
   install_framework 'JSQMessagesViewController.framework'
   install_framework 'JSQSystemSoundPlayer.framework'
