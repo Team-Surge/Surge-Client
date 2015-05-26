abcd342b5c8407997863f62dfdbf65ec39328f53
omoman <mannysanchez998@gmail.com>
2015-05-25 17:43:15 -0700

    Home: Add poll creation

--- Surge-Client/YakCreatePostViewController.swift
+++ Surge-Client/YakCreatePostViewController.swift
@@ -19,9 +19,19 @@ class YakCreatePostViewController: UIViewController {
   @IBOutlet weak var handleField: UITextField!
   @IBOutlet weak var bottomConstraint: NSLayoutConstraint!
   
+  @IBOutlet weak var optionOneField: UITextField!
+  @IBOutlet weak var optionTwoField: UITextField!
+  @IBOutlet weak var optionThreeField: UITextField!
+  @IBOutlet weak var optionFourField: UITextField!
+  
   override func viewDidLoad() {
     textView.delegate = self
     handleField.delegate = self
+    optionOneField.delegate = self
+    optionTwoField.delegate = self
+    optionThreeField.delegate = self
+    optionFourField.delegate = self
+    
     NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("keyboardWillShow:"), name:UIKeyboardWillShowNotification, object: nil)
     NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("keyboardWillHide:"), name:UIKeyboardWillHideNotification, object: nil)
     super.viewDidLoad()
@@ -55,31 +65,82 @@ class YakCreatePostViewController: UIViewController {
   }
   
   func hasValidPostText () -> Bool {
+    // Validate handle
     if count(handleField.text) > 15 {
       SurgeToast.showError("Handle must be less than 15 characters", onCompletion: ({_ in}))
       return false
-    } else if count(textView.text) == 0 {
+    }
+    
+    // Validate post text content
+    if count(textView.text) == 0 {
       SurgeToast.showError("Body can't be empty", onCompletion: ({_ in}))
       return false
     }
+    
+    // Validate poll options
+    let pollFieldContents = getOptionFields()
+    
+    for i in 0..<pollFieldContents.count {
+      for j in 0...i {
+        if pollFieldContents[i] != "" && pollFieldContents[j] == "" {
+          SurgeToast.showError("Missing option field", onCompletion: ({_ in}))
+          return false
+        }
+      }
+    }
+    
     return true
+  }
+  
+  func getOptionFields() -> [String] {
+    return [
+      optionOneField.text,
+      optionTwoField.text,
+      optionThreeField.text,
+      optionFourField.text
+    ]
   }
   
   func createPostAction() {
     let request = HTTPTask()
     let lastLocation = LocationManager.sharedInstance().lastLocation
-    let params: [String:AnyObject] = ["action": "postCreate", "handle": handleField.text, "content": textView.text, "lat": toString(lastLocation.coordinate.latitude), "lng": toString(lastLocation.coordinate.longitude)]
+    var params: [String:AnyObject] = ["action": "postCreate", "handle": handleField.text, "content": textView.text, "lat": toString(lastLocation.coordinate.latitude), "lng": toString(lastLocation.coordinate.longitude)]
     
     if hasValidPostText() == false {
       return
     }
     
-    request.POST("http://surge.seektom.com/post", parameters: params,
-      success: {(response: HTTPResponse) in
-      }, failure: {(error: NSError, response: HTTPResponse?) in
-        SurgeToast.showError("Failed to create post", onCompletion: ({_ in}))
+    let postFieldContents = getOptionFields()
+    if postFieldContents[0] != "" {
+      // Poll post
+      var numFilledOptions = 0
+      for i in postFieldContents {
+        if i != "" {
+          numFilledOptions += 1
+        }
       }
-    )
+      // Add poll options to params
+      params["type"] = "poll"
+      params["pollOptionCount"] = numFilledOptions
+      for i in 1...4 {
+          params["pollOption\(i)"] = postFieldContents[i - 1]
+      }
+      
+      request.POST("http://surge.seektom.com/post", parameters: params,
+        success: {(response: HTTPResponse) in
+        }, failure: {(error: NSError, response: HTTPResponse?) in
+          SurgeToast.showError("Failed to create post", onCompletion: ({_ in}))
+        }
+      )
+    } else {
+      // Standard post
+      request.POST("http://surge.seektom.com/post", parameters: params,
+        success: {(response: HTTPResponse) in
+        }, failure: {(error: NSError, response: HTTPResponse?) in
+          SurgeToast.showError("Failed to create post", onCompletion: ({_ in}))
+        }
+      )
+    }
     navigationController?.popViewControllerAnimated(true)
   }
 }
